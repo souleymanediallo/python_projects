94c82a070f435278deda4550c12ce275ea60090d
Souleymane Diallo <souleymanedialloo@yahoo.fr>
2022-04-03 12:12:07 +0000

    operations

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-import random
+def add(n1, n2):
+    return n1 + n2
 
-nombre_mystere = random.randint(0, 10)
-nombre = input("Quel est le nombre mystere ? : ")
 
-if nombre.isdigit():
-    nombre = int(nombre)
-    if nombre > nombre_mystere:
-        print(f"Le nombre mystere est plus petit que {nombre}")
-    elif nombre < nombre_mystere:
-        print(f"Le nombre mystere est plus grand que {nombre}")
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    if n2 > 0:
+        return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number "))
+
+for symbol in operations:
+    print(symbol)
+
+should_continue = True
+
+while should_continue:
+    operation_symbol = input("Pick an operation from the line above : ")
+    num2 = int(input("What's the second number "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == "y":
+        num1 = answer
     else:
-        print("Bravo, vous avez trouvé le nombre mystère !")
-else:
-    print("SVP, entrez un nombre valide.")
-
-
-    
+        should_continue = False
